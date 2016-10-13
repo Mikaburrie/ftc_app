a1a85e7b9904e199810aea7f746739f41760a4a0
innanill <inna.nill@gmail.com>
2016-10-13 19:22:07 -0400

    Added missing file.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedContainer.java
@@ -0,0 +1,31 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by nill on 2/18/16.
+ */
+public class SpeedContainer {
+    private double rightSpeed;
+    private double leftSpeed;
+
+    public SpeedContainer()
+    {
+        this.rightSpeed = 0;
+        this.leftSpeed = 0;
+    }
+
+    public double getLeftSpeed() {
+        return leftSpeed;
+    }
+
+    public void setLeftSpeed(double leftSpeed) {
+        this.leftSpeed = leftSpeed;
+    }
+
+    public double getRightSpeed() {
+        return rightSpeed;
+    }
+
+    public void setRightSpeed(double rightSpeed) {
+        this.rightSpeed = rightSpeed;
+    }
+}
