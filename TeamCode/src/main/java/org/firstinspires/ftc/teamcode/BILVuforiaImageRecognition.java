7af6281434c07c5a93785dd1b3c6fbf5410fb320
Mikaburrie <mikaburrie@gmail.com>
2016-11-08 10:24:33 -0500

    Add to BILVuforiaImageRecognition.java to track up to 4 of the images that will be shown under the beacons.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILVuforiaImageRecognition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILVuforiaImageRecognition.java
@@ -2,10 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import java.util.ArrayList;
@@ -22,27 +27,46 @@ public class BILVuforiaImageRecognition extends LinearOpMode {
 
     @Override public void runOpMode() throws InterruptedException {
         //Sets up camera and initializes vuforia.
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(com.qualcomm.ftcrobotcontroller.R.id.cameraMonitorViewId);
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         parameters.vuforiaLicenseKey =
                 "AU6p3vX/////AAAAGXbGGUU5NkaliUUGq2/Kz/Z6xyNBL1fmMylvq1uMMqkFx8J4CCcF4S34r9tecpyQ0E4VJjgRklceKPzDOpiiQFxzNkZxSiIwTx4zylpK8pf6oBUg6qXji/9xlE41OyFzhKkjgCJekACepkKmviiUF94oSIRvNjOqGlVVtOKCUN3LTYF/xTIULB52NVbaXvco/9alDrM6diA/Vdfdd0qVxDnZ8mu4R7PsO19lJNcpEyNs4eSjvPlY3t75KJM1sHFFl3nUn5Piggv65VU5+sfS6VELGrGDNisMLFsp3Qk8+KSV5twxoM5cnpjGqN1CYusvkhyJAF/TBXNKb0LN0b2PNkVPIU1VYeycNax0z6HHZZ6J";
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK; //uses the back camera(higher resolution)
+        parameters.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters); //creates new vuforia class with parameters
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4); //the max amound of image targets we are looking for
 
-        VuforiaTrackables imageTargets = vuforia.loadTrackablesFromAsset("FTC_2016-17");
-        VuforiaTrackable wheelTarget = imageTargets.get(0);
-        wheelTarget.setName("Wheels");
+        VuforiaTrackables imageTargets = vuforia.loadTrackablesFromAsset("FTC_2016-17"); //gets the targets from assets
+        imageTargets.get(0).setName("Wheels"); //name first one wheels
+        imageTargets.get(1).setName("Tools"); //second is tools
+        imageTargets.get(2).setName("Legos"); //third is legos
+        imageTargets.get(3).setName("Gears"); //fourth is gears
 
-        VuforiaTrackable toolTarget = imageTargets.get(1);
-        toolTarget.setName("Tools");
+        waitForStart(); //waits for the op mode to be started
 
-        VuforiaTrackable legoTarget = imageTargets.get(2);
-        legoTarget.setName("Legos");
+        imageTargets.activate(); //activate the tracking of the image targets once the opmode starts
 
-        VuforiaTrackable gearTarget = imageTargets.get(3);
-        gearTarget.setName("Gears");
+        while(opModeIsActive()){ //when the op mode is active
+            for(VuforiaTrackable beaconImage : imageTargets){ //loop throught all of the trackables
+                OpenGLMatrix position = ((VuforiaTrackableDefaultListener) beaconImage.getListener()).getPose(); //get positions
+
+                if(position != null){ //if we see the object we are looking for
+                    VectorF translation = position.getTranslation();
+
+                    telemetry.addData(beaconImage.getName() + " - Translation", translation);
+
+                    double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2))); //vertical phone
+
+                    telemetry.addData(beaconImage.getName() + " - Degrees", degreesToTurn);
+                }else{
+                    telemetry.addData(beaconImage.getName(), "Not In View");
+                }
+            }
+            telemetry.update();
+        }
+
 
         /** For convenience, gather together all the trackable objects in one easily-iterable collection */
-        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+/*        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
         allTrackables.addAll(imageTargets);
 
 
@@ -50,6 +74,6 @@ public class BILVuforiaImageRecognition extends LinearOpMode {
         float mmBotWidth       = 18 * mmPerInch;            // ... or whatever is right for your robot
         float mmFTCFieldWidth  = (12*12 - 2) * mmPerInch;   // the FTC field is ~11'10" center-to-center of the glass panels
 
-
+        */
     }
 }
