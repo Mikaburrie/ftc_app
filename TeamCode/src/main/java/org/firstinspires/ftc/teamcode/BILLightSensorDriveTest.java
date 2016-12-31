471ef0adffded9e7b265c3d9e78fc7ee2ce41ef2
ZohaPeterson <Zohapeterson@gmail.com>
2016-12-31 11:42:45 -0500

    Made BILLightSensorDriveTest.java to test the color sensor with the line

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILLightSensorDriveTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Made by Zoha Peterson on 12/31/16.
+ */
+@Autonomous(name="BIL: Light Sensor Drive Test", group="BIL")
+    public class BILLightSensorDriveTest extends LinearOpMode{
+
+        BILRobotHardware robot = new BILRobotHardware();
+
+
+        @Override
+        public void runOpMode() throws InterruptedException {
+
+            robot.init(hardwareMap);
+
+            double darkFloorValue = robot.lightSensor.getLightDetected();
+            while(!isStarted()) {
+                darkFloorValue = (darkFloorValue + robot.lightSensor.getLightDetected())/2;
+
+                //continuously calibrate gyro to keep the heading as accurate as possible
+                if(!robot.gyroSensor.isCalibrating()){
+                    robot.gyroSensor.calibrate();
+                }
+
+                idle();
+            }
+
+//            waitForStart();
+            robot.driveUntilLineOrDistance(0.5, 5, darkFloorValue);
+
+
+        }
+    }
+
