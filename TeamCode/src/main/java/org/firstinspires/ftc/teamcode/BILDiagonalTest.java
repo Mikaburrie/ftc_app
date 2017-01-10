9133ac1e36a0b239623a0f84e0ed853b89cf4872
Mikaburrie <mikaburrie@gmail.com>
2017-01-10 18:39:11 -0500

    Added test to make the robot drive diagonally.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILDiagonalTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created on 1/10/2017 by Mika.
+ */
+@Autonomous(name="BIL: Diagonal Test", group="BIL")
+public class BILDiagonalTest extends BILAutonomousCommon {
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        setDriveMotors(0, -1, -1, 0);
+
+        time.reset();
+        while (time.milliseconds() < 2000) {
+            idle();
+        }
+
+        setAllDriveMotors(0);
+
+    }
+}
