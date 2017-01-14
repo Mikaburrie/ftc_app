a04704898889f8161ac423732fb0d69f4a125332
Mikaburrie <mikaburrie@gmail.com>
2017-01-14 12:03:33 -0500

    Changed TimedAutonomousLinear to CapBall to knock the capball off

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILCapBall.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+/**
+ * Created by mikab_000 on 10/20/2016.
+ */
+@Autonomous(name="BIL: Cap Ball", group="BIL")
+//@Disabled
+public class BILCapBall extends BILAutonomousCommon {
+    /* Declare OpMode members. */
+    BILRobotHardware robot      = new BILRobotHardware();   // Use BIL Robot's hardware
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        driveDistance(0.5, 4);
+    }
+}
