81a8948445bd903c09ce55f062431711672a6461
Mikaburrie <mikaburrie@gmail.com>
2017-04-07 18:45:50 -0400

    Got rid of idle()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILVuforiaGearsPicTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BILVuforiaGearsPicTest.java
@@ -49,7 +49,7 @@ public class BILVuforiaGearsPicTest extends BILAutonomousCommon {
                 }
             }
             telemetry.update();
-            //idle();
+            //qidle();
         }
     }
 }
